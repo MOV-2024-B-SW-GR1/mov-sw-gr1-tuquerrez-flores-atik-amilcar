ba1e7e10bd3684101acb2c58bdb09af1ef1eb790
Atik T <atiktuquerres@gmail.com>
2024-12-03 22:01:53 -0500

    add: clase 11

---
+++ 01-Andrioid/app/src/main/java/com/example/sw2024bgr1_aatf/ACicloVida.kt
@@ -0,0 +1,79 @@
+package com.example.sw2024bgr1_aatf
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+
+class ACicloVida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_aciclo_vida)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_ciclo_vida)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        mostrarSnackbar("OnCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mostrarSnackbar("OnCStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mostrarSnackbar("OnResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        mostrarSnackbar("OnRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mostrarSnackbar("OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mostrarSnackbar("OnStop")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run {
+            //Guardar las variables
+            putString("variableTextoGuardado ",textoGlobal)
+        }
+
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        //Recuperar variable
+        val textoRecuperado: String? = savedInstanceState
+            .getString("variableTextoGuardado")
+        if(textoRecuperado != null){
+           mostrarSnackbar(textoRecuperado)
+        }
+    }
+
+    var textoGlobal =""
+
+    fun mostrarSnackbar(text:String){
+        textoGlobal += text
+        val snack = Snackbar.make(
+            findViewById(R.id.cl_ciclo_vida),
+            textoGlobal,
+            Snackbar.LENGTH_INDEFINITE
+        )
+        snack.show()
+    }
+}
