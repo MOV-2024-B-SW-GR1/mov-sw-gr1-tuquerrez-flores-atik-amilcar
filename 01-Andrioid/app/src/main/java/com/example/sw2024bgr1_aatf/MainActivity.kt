fbba6f271727c8087b9188b12aea3201b25602df
Atik T <atiktuquerres@gmail.com>
2025-01-24 19:18:08 -0500

    Clase 18

--- 01-Andrioid/app/src/main/java/com/example/sw2024bgr1_aatf/MainActivity.kt
+++ 01-Andrioid/app/src/main/java/com/example/sw2024bgr1_aatf/MainActivity.kt
@@ -123,6 +123,12 @@ class MainActivity : AppCompatActivity() {
                 irActividad(FRecyclerView::class.java)
             }
 
+        val botonGMaps = findViewById<Button>(R.id.btn_google_maps)
+        botonGMaps
+            .setOnClickListener {
+                irActividad(GGoogleMaps::class.java)
+            }
+
     }
     fun irActividad(clase:Class<*>){
         startActivity(Intent(this, clase))
